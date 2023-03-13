a5d6cb264e57ac7b3ed58fd1151e27dffbc830fc
hanae-1029 <nichibi1029@icloud.com>
2023-03-13 16:12:40 +0900

    コミット

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,14 +1,23 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
 
     @GetMapping("/input")
-    public String getinput() {
+    public String getInput() {
+        return "input";
 
+    }
+
+    @PostMapping("/input")
+    public String input(@RequestParam("previous") String previous, Model madel) {
+        madel.addAttribute("previous", previous);
         return "input";
     }
 
--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -2,22 +2,16 @@ package com.techacademy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OutputController {
 
-    @GetMapping("/input")
-    public String getinput() {
-        return "input";
-    }
-
-    @PostMapping("/input")
-    public String postOutput(@RequestParam("val") String val, Model model) {
-        model.addAttribute("val", val);
-        return "input";
+    @PostMapping("/output")
+    public String postOutput(@RequestParam("previous") String previous, Model madel) {
+        madel.addAttribute("previous", previous);
+        return "output";
     }
 
 }
